1cb2f355dfe79bc8d7247a50f8590fbbeb0f6b89
HPhuc122 <phuc33145@gmail.com>
2025-04-27 16:48:46 +0700

    auth.py

--- MainCode/routes/auth.py
+++ MainCode/routes/auth.py
@@ -30,9 +30,10 @@ def rotate_image(image, angle):
     return rotated
 
 # Hàm trích đặc trưng có xoay ảnh
-def extract_augmented_features(image_path, angles=None):
+def extract_augmented_features(image_input, angles=None):
     if angles is None:
         angles = [-15, -12, -9, -6, -3, 0, 3, 6, 9, 12]
+    
     # Nếu truyền vào là đường dẫn file
     if isinstance(image_input, str):
         image = cv2.imread(image_input)
@@ -40,8 +41,10 @@ def extract_augmented_features(image_path, angles=None):
             raise ValueError(f"Không đọc được ảnh từ đường dẫn: {image_input}")
     else:
         image = image_input
-    image = cv2.cvtColor(image_path, cv2.COLOR_BGR2RGB)
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = cv2.resize(image, (224, 224))
+
     feature_list = []
 
     for angle in angles:
